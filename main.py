698c3c59e14a0fdd97bcf3c41b397fe01fa17caa
Haralds Ķempelis <haralds.kempelis@edu.rtu.lv>
2023-02-10 14:30:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,23 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.insert(0,Bracket(next, i + 1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
-
+            if len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[0].char, next) != True:
+                return i + 1
+            opening_brackets_stack.pop(0)
+            
+    return "Success"
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    if(mismatch == True):
+        print("Success")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
