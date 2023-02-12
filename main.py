67e62d26107d7dcdcbbf6897fdefd3c98d9da240
Haralds Ķempelis <124581096+HarKemp@users.noreply.github.com>
2023-02-12 16:57:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def find_mismatch(text):
 def main():
     text = input()
     if(text.find('I') != -1):
+        text = input()
         mismatch = find_mismatch(text)
         if(mismatch == "Success"):
             print("Success")
